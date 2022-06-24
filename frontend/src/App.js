daf22f9cb3f85e704c9dcfb00028961b41ecc759
Nina <labferment10@gmail.com>
2022-06-25 00:52:42 +0200

    app deployed to the internet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,9 +12,11 @@ const App = () => {
 	const [errorMessage, setErrorMessage] = useState('');
 
 	useEffect(() => {
-		axios.get('http://localhost:3001/api/notes').then((res) => {
-			setNotes(res.data);
-		});
+		axios
+			.get('https://secret-tundra-71870.herokuapp.com/api/notes')
+			.then((res) => {
+				setNotes(res.data);
+			});
 	}, []);
 
 	const addNote = (event) => {
